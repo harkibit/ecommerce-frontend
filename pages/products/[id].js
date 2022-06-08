77c401857501e4b99f32dc26fe000fd4ab122b02
Hiba Abdel Karim <abdelkarim.hiba@outlook.com>
2022-06-08 19:18:17 +0300

    added counter to single product and added the product page layout

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,18 +1,32 @@
 import React, { useState } from "react";
 import Head from "next/head";
-
+import Image from "next/Image";
 import { useRouter } from "next/router";
 
-import { API_URL } from "../../utils/urls";
+import { BsCartPlus } from "react-icons/bs";
+import { HiMinus, HiPlus } from "react-icons/hi";
+
+import Navbar from "../../components/Navbar";
+import Layout from "../../components/Layout";
+
+import styles from "../../styles/Home.module.css";
+import { API_URL, fromImageToURL } from "../../utils/urls";
+import { formatDecimals } from "../../utils/format";
 
 export default function Product() {
+  const [counter, setCounter] = useState(1);
   const [product, setProduct] = useState({});
   const router = useRouter();
   const { id } = router.query;
 
+  const decreaseCounter = () => {
+    counter > 1 && setCounter(counter - 1);
+  };
+  const increaseCounter = () => setCounter(counter + 1);
+
   React.useEffect(() => {
     id &&
-      fetch(API_URL + "products/" + id)
+      fetch(API_URL + "products/" + id + "?populate=*")
         .then((res) => res.json())
         .then((data) => {
           setProduct(data.data);
@@ -37,7 +51,45 @@ export default function Product() {
         />
         <link rel="icon" href="favicon.png" />
       </Head>
-      <div>{product?.attributes?.title}</div>
+      <Navbar />
+      <Layout>
+        <div className={styles.productContainer}>
+          <div>
+            <Image
+              src={fromImageToURL(product?.attributes?.image.data?.attributes)}
+              alt={product?.attributes?.title}
+              width={600}
+              height={600}
+              className={styles.productImage}
+              style={{ borderRadius: "10px" }}
+            />
+          </div>
+          <div className={styles.productContent}>
+            <span className={styles.productGenre}>
+              {product?.attributes?.genre}
+            </span>
+            <h1>{product?.attributes?.title}</h1>
+            <p>{product?.attributes?.description}</p>
+            <span className={styles.productPrice}>
+              ${formatDecimals(product?.attributes?.price, 2)}
+            </span>
+            <div className={styles.productFooter}>
+              <div className={styles.productCounterContainer}>
+                <button className={styles.iconBtn} onClick={decreaseCounter}>
+                  <HiMinus />
+                </button>
+                <span className={styles.counter}>{counter}</span>
+                <button className={styles.iconBtn} onClick={increaseCounter}>
+                  <HiPlus />
+                </button>
+              </div>
+              <button className={`${styles.btn} ${styles.btnPrimary}`}>
+                <BsCartPlus /> &nbsp; Add to Cart
+              </button>
+            </div>
+          </div>
+        </div>
+      </Layout>
     </>
   );
 }
